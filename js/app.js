e00ae17fab243f6ad78eb9a129036e818690d334
8GutiMu <you@example.com>
2017-06-12 09:29:15 -0500

    generando un error al no poner la url correcta

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var cargarPagina = function () {
 };
 
 var cargarPersonajes = function () {
-    $.ajax('http://swapi.co/api/people/',{
+    $.ajax('http://swapi.com/api/people/',{
         method: 'GET',
         dataType: 'json',
         success: function (response) {
